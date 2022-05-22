c0f31b034f5773bdce2dff730632cf5052fcba86
Victor Haynes <haynesvic@gmail.com>
2022-05-21 20:20:45 -0500

    commit tree 2db1757cea075c6fb2823f3c5c052128c9fb56d2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,
+    [key]: value,
+    };
+}
+
+
+//DestructivelyUpdateEmployeeWithKeyAndValue()
+//Note: you must use Bracket Notation, if you use Dot Notation
+//The "employee.key" key will be taken literally, not evaluated
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return(employee)
+}
+
+//deleteFromEmployeeByKey()
+function deleteFromEmployeeByKey(employee,key){
+    const noKeyEmployee = {...employee}
+    delete noKeyEmployee[key]
+    return(noKeyEmployee)
+}
+
+//destructivelyDeleteFromEmployeeByKey()
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return(employee)
+}
